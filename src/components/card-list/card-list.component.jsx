ff2ee719bb3b26ecf415517a28ef48c1b992d354
PlanetNamekTech <sr23513@gmail.com>
2022-06-22 12:48:20 -0700

    finished Class Component styling

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,28 +1,21 @@
 import { Component } from "react";
 import './card-list.styles.css';
+import Card from '../card/card.component'
 
 class CardList extends Component {
     // super();
 
     render() {
-        const { monsters } = this.props;
+        const {monsters} = this.props;
+        return ( 
+            <div className="card-list">
+                {monsters.map((monster) => {
+                    return (
+                        <Card monster={monster}/>
+                    )
+                })}
+            </div>   
         
-        return (    
-        <div className='card-list'>
-            {monsters.map((monster) => {
-                const { id, name, email } = monster;
-                return(
-                <div className="card-container" key={id}>
-                    <img 
-                        src={`https://robohash.org/${id}?set=set2&size=180x180`} 
-                        alt={`monster ${name}`}
-                     />
-                    <h2>{name}</h2>
-                    <p>{email}</p>
-                </div>
-                
-            )})}
-        </div>
         );
     }
 }
